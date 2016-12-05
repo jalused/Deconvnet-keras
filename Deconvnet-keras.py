6e729affe53b28f479fb5ee6753b8cbef865b612
Liang Jiang <jal@mail.ustc.edu.cn>
2016-12-05 20:38:30 +0800

    fix bug

--- Deconvnet-keras.py
+++ Deconvnet-keras.py
@@ -17,6 +17,7 @@ from keras.layers import (
         Input,
         InputLayer,
         Flatten,
+        Activation,
         Dense)
 from keras.layers.convolutional import (
         Convolution2D,
@@ -423,6 +424,8 @@ def visualize(model, data, layer_name, feature_to_visualize, visualize_mode):
             deconv_layers.append(DDense(model.layers[i]))
             deconv_layers.append(
                     DActivation(model.layers[i]))
+        elif isinstance(model.layers[i], Activation):
+            deconv_layers.append(DActivation(model.alyers[i]))
         elif isinstance(model.layers[i], Flatten):
             deconv_layers.append(DFlatten(model.layers[i]))
         elif isinstance(model.layers[i], InputLayer):
